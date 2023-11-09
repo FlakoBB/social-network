0fb7c26d7b883a62cffbe68980d39c47a3a6cbb8
FlakoBB <josephrbb99@gmail.com>
2023-11-09 15:21:31 -0600

    add: SideMenu component | images added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Inconsolata } from 'next/font/google'
 import '@/styles/globals.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+const inconsolata = Inconsolata({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +10,8 @@ export const metadata = {
 
 export default function RootLayout ({ children }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='es'>
+      <body className={inconsolata.className}>{children}</body>
     </html>
   )
 }
