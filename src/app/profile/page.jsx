5f5e92cdeca025a5394ff1658fc94b92cf3afe06
Julio Patiño <franjp132@gmail.com>
2023-11-20 17:51:04 -0500

    fix: change the width styles for the profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,10 @@
 import ProfileFeet from '@/components/ProfileFeet'
 import SideMenu from '@/components/SideMenu'
+import styles from '@/styles/profileFeet.module.scss'
 
 const PageProfile = () => {
   return (
-    <main className='mainContainer'>
+    <main className={styles.mainContainer}>
       <SideMenu />
       <ProfileFeet />
     </main>
